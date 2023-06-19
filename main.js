c4909b162d743297eb5adfbfc5f3214a94dcdca4
Gabriela Kleinschmitt Padilha <134091051+GabrielaKP05@users.noreply.github.com>
2023-06-19 16:12:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ const pecas = {
 }
 
 controle.forEach (elemento) => {
-    elemento.addEventListener('click', (evento) => {
+    elemento.addEventListener'click', (evento) => {
         manipulaDados(evento.target.textContent, evento.target.parentNode)
         atualizaEstatistica(evento.target.dataset.controle)
     })
